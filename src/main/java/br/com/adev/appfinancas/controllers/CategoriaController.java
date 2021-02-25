d26fced0eab41fc8a561c73407956ab33abc0dfd
andrer54 <andrer54@gmail.com>
2021-02-25 07:20:26 -0300

    add bootstrap

--- src/main/java/br/com/adev/appfinancas/controllers/CategoriaController.java
+++ src/main/java/br/com/adev/appfinancas/controllers/CategoriaController.java
@@ -6,19 +6,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.adev.appfinancas.model.Categoria;
+import br.com.adev.appfinancas.model.Transacao;
 import br.com.adev.appfinancas.repository.CategoriaRepository;
+import br.com.adev.appfinancas.repository.TransacaoRepository;
 
 @Controller
 public class CategoriaController {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private TransacaoRepository tr;
 
     @RequestMapping("/categorias")
     public ModelAndView listaCategorias(){
@@ -37,4 +43,35 @@ public class CategoriaController {
         categoriaRepository.save(categoria);
         return "redirect:/";
     }
+
+    @RequestMapping(value="/categoria/{id}", method=RequestMethod.GET)
+    public ModelAndView detalhesCategoria(@PathVariable("id") long id){
+        Categoria categoria = categoriaRepository.findById(id);
+        ModelAndView mv = new ModelAndView("/detalhesCategoria");
+        mv.addObject("categoria", categoria);
+
+        Iterable<Transacao> transacoes = tr.findByCategoria(categoria);
+        mv.addObject("transacoes", transacoes);
+        return mv;
+    }
+ 
+
+    @RequestMapping(value="/categoria/{id}", method=RequestMethod.POST)
+    public String detalhesCategoriaPost(@PathVariable("id") long id, Transacao transacao, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique os campos");
+            return "redirect:/categoria/{id}";
+        }
+        Categoria categoria = categoriaRepository.findById(id);
+        transacao.setCategoria(categoria);
+        tr.save(transacao);
+        //esse trecho deve subtrair do saldo da conta.
+        //conta.setSaldo(conta.getSaldo() - transacao.getValor());
+        //cr.save(conta);
+
+        attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
+            
+        return "redirect:/categoria/{id}";
+    }
+
 }
